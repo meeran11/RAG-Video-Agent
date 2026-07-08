18ed8fd85a40e51b5083321f7050e0b61b934efd
meeran11 <meeran.official1255@gmail.com>
2026-07-08 17:30:52 +0500

    Added summarizer to summarize the generated transcript

---
+++ core/summarize.py
@@ -0,0 +1,68 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough,RunnableLambda
+import os
+
+def get_llm():
+    return ChatMistralAI(
+        model = "mistral-small-latest",
+        mistral_api_key = os.getenv("MISTRAL_API_KEY"),
+        temperature=0.2
+        )
+
+def split_transcript(transcript:str) ->list :
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 10
+    )   
+
+    return splitter.split_text(transcript)
+
+def summarize(transcript:str)->str:
+    llm = get_llm()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system","Summarize this portion of a transcription concisely."),
+            ("human","{text}")
+        ]
+    )
+
+    parser = StrOutputParser()
+
+    chain = prompt | llm | parser
+
+    chunks = split_transcript(transcript)
+
+    chunk_summaries = [chain.invoke({"text",chunk}) for chunk in chunks]
+
+    summary = "\n\n".join(chunk_summaries)
+
+    combined_prompt = ChatPromptTemplate.from_messages(
+        [
+        (
+            "system",
+            "You are an expert meeting summarizer. Combine these partial summaries "
+            "into one final professional meeting summary in bullet points.",
+        ),
+        ("human", "{text}"),
+    ]
+    )
+
+    combined_chain = combined_prompt | llm | StrOutputParser()
+
+    return combined_chain.invoke({"text": summary})
+
+def generate_title(transcript:str)->str:
+    llm = get_llm()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Based on the meeting transcript, generate a short professional meeting title "
+                "(max 8 words). Only return the title, nothing else."),
+            ("human","{text}")
+        ]
+    )
+    chain = prompt | llm | StrOutputParser()
+
+    return chain.invoke(transcript[:200])
